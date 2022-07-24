28a7da9e9fe8fad81127c12d042cc6b16c64952f
rajshree516 <rajshreesomani516@gmail.com>
2022-07-25 03:45:28 +0530

    added

--- 13_React/movieapp/src/components/MoviesList.js
+++ 13_React/movieapp/src/components/MoviesList.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react'
 
-import{movies} from "../movieData";
+import{movies} from '../movieData';
 
 export class MoviesList extends Component {
   render() {
     let moviesArr=movies.results
-    console.log(moviesArr)
-    return (
-      // <div>
-      //   {
-      //     moviesArr.map((movieElem)=>(
-      //       <h1>{movieElem.title}</h1>
-      //     ))
-      //   }
-      // </div>
 
-      
+    return (
+      <>
+      <div>
+        <h3 className='text-center'><strong>Trending</strong></h3>
+      </div>
+
+      <div className='movies-list'>
+        {
+          moviesArr.map((movieEle)=>(
+            <div className="card movie-card" >
+            <img src="https://m.media-amazon.com/images/M/MV5BMjMxMzc4NTEyN15BMl5BanBnXkFtZTgwNDgyMjA3MTE@._V1_FMjpg_UX1000_.jpg" style={{height:'40vh' , width:'20vw'}} className="card-img-top movie-img" alt="..."/>
+    
+            <h5 className="card-title banner-title">{movieEle.title}</h5>
+            {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+     
+            </div>
+          ))
+        }
+
+      </div>
+      </>
     )
   }
 
